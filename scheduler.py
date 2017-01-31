6622edc3e0d8eeb1f1bbe90ab4860a91be6bd0e5
zimonbizkit <simon.gallegos.edu@gmail.com>
2017-01-31 21:48:49 +0100

    Initial version of simple scheduler

---
+++ scheduler.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+import Queue
+import sys
+class Job(object):
+    def __init__(self, priority, description):
+        self.priority = priority
+        self.description = description
+        print 'New job:', description
+        return
+        
+    def __cmp__(self, other):
+        return cmp(self.priority, other.priority)
+
+
+
+q = Queue.PriorityQueue()
+
+
+_in = ""
+while _in != 'end':
+    print "Please input system job name (or command):"
+    _in = raw_input()
+    print "And please input priority:"
+    _priority = sys.stdin.readline()
+    q.put(Job(_priority,_in))
+
+while not q.empty():
+    next_job = q.get()
+    print 'Processing job:', next_job.description
+                                                                    
