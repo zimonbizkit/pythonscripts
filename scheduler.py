c98917414f12f9e4994ec921a42ea8a8fdb459fd
zimonbizkit <simon.gallegos.edu@gmail.com>
2017-01-31 22:17:29 +0100

    Added default functionality for executing bash commands

--- scheduler.py
+++ scheduler.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 import Queue
 import sys
+import os
 from time import sleep
 class Job(object):
-    def __init__(self, priority, description):
+    def __init__(self, priority, description,kind):
         self.priority = priority
         self.description = description
-        print 'New job:', description
+        self.kind= kind
+        print 'New job-->',description,kind
         return
         
     def __cmp__(self, other):
@@ -19,13 +21,24 @@ q = Queue.PriorityQueue()
 
 _in = ""
 while _in != 'end':
+    _kind=''
+    print "Please input type of command (either command or just string):"
+    _kind=raw_input()
+    while _kind!='string' and _kind!='command':
+        print "Damn, you are wrong. Input either string or command!"
+        _kind=raw_input()
+
     print "Please input system job name (or command):"
     _in = raw_input()
     print "And please input priority:"
     _priority = sys.stdin.readline()
-    q.put(Job(_priority,_in))
+    q.put(Job(_priority,_in,_kind))
 
 while not q.empty():
     next_job = q.get()
-    print 'Processing job:', next_job.description
+    if next_job.kind == "string":
+        print 'Processing job:PRINTING-->', next_job.description
+    elif next_job.kind =="command":
+        print 'Processing job:EXECUTING-->',next_job.description
+        os.system(next_job.description);
     sleep(0.5)                                                                  
